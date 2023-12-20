41e460441b0bd20e5d60181d0eac9768b01a7df0
Yusen Hu <richardyusen@gmail.com>
2023-12-20 17:38:23 +0000

    Wireframe rectangle

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ int main() {
     glfwDefaultWindowHints();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    // OSX only?
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
@@ -56,10 +57,17 @@ int main() {
     glGenVertexArrays(1, &VAO); 
     glBindVertexArray(VAO);
 
+    // Four corners of a square.
     float pos[] = {
-        -0.5f, -0.5f,
-        0.5f, -0.5f,
-        0.0f,  0.5f,
+        0.5, 0.5,
+        -0.5, 0.5,
+        -0.5, -0.5,
+        0.5, -0.5,
+    };
+
+    uint idx[] = {
+        0, 1, 2,    // 1st triangle
+        0, 2, 3,    // 2nd triangle
     };
 
     uint VBO;
@@ -82,13 +90,26 @@ int main() {
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
                           2*sizeof(float), (const void*)0);
+    
+    uint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx), idx, GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+    glDisableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // Don't fill (i.e. draw a wireframe)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (glfwWindowShouldClose(window) == 0) {
         glClear(GL_COLOR_BUFFER_BIT);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
