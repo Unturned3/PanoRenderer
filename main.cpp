ced1caeef28c364b4cd069daf01f51bc51dc67d6
Yusen Hu <richardyusen@gmail.com>
2024-02-12 11:54:57 +0000

    Fix compiler error for GLM 1.0.0

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@
 #include <cassert>
 #include <cmath>
 #include <cstdint>
+
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_access.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/random.hpp>
 #include <glm/gtx/string_cast.hpp>
+
 #include <iostream>
 #include <memory>
 #include <string>
