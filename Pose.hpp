a80399cc942f93791a60369733778524515a2c09
Yusen Hu <richardyusen@gmail.com>
2024-05-07 22:10:05 +0100

    Finalize yaw/pitch/roll conventions & clarify explanation

--- Pose.hpp
+++ Pose.hpp
@@ -19,31 +19,27 @@ void updatePose()
     if (s.enable_trajectory) {
         double *p = data + static_cast<size_t>(s.pose_idx) * arr.shape[1];
 
-        /*  NOTE: traj.py uses X forward, Y right, Z down for the camera.
-            However, we are using -Z forward, X right, Y up for the camera.
-            So, in order to get the same rendering effect as the ones
-            produced by traj.py, we will negate the yaw values here.
+        /*  NOTE: cv2 imread, imwrite, VideoCapture, and VideoWriter all
+            assumes a top-left texture origin. When they load a correctly
+            oriented image from disk, the top-left corner of the image is (0,0).
+            When they write an image to disk, they assume the input image's
+            origin is at the top-left.
 
-            NOTE: screw it. We're gonna use this OpenGL program's setup as the
-            convention for rendering things.
-        */
+            However, OpenGL uses bottom-left as the texture origin. So, it
+            will display an upright image loaded by cv2 with the y-axis flipped.
+            Similarly, if an upright image in OpenGL's framebuffer is written to
+            disk using cv2, it will be flipped in the y-axis again.
+
+            Flipping images on the CPU is slow. Therefore, we never flip them,
+            and OpenGL will work with the y-flipped images. However, this means
+            that we need to negate our pitch and roll angles. Or else, pitching up in
+            OpenGL will actually be pitching down in the final output video (and
+            rolling clockwise in-screen will appear as a counter-clockwise
+            rotation). */
+
         float yaw = static_cast<float>(p[0]);  // pan
-
-        /*  NOTE: OpenGL assumes bottom-left texture origin, while most other
-            libraries (e.g. OpenCV) assumes upper-left. So, the frames loaded
-            by OpenCV are upside-down when rendered in OpenGL. Previously, we
-            render the frames correctly oriented to the window by changing every
-            texture v coordinate to (1-v). However, this means the frames are
-            "flipped" from the perspective of cv::VideoWriter when we read it
-            back from the window with glReadPixels.
-
-            So, for the sake of video codec efficiency, we don't do any frame
-            flipping (at the cost of rendering them upside-down to the window).
-            However, we also need to negate the pitch values correspondingly,
-            as done below.
-        */
         float pitch = -1 * static_cast<float>(p[1]);     // tilt
-        float roll = static_cast<float>(p[2]);
+        float roll = -1 * static_cast<float>(p[2]);
         float fov = static_cast<float>(p[3]);
 
         s.pan = yaw, s.tilt = pitch, s.roll = roll;
@@ -57,7 +53,7 @@ void updatePose()
         R = glm::rotate(R, glm::radians(yaw), {0, 1, 0});
         R = glm::rotate(R, glm::radians(pitch), {1, 0, 0});
         // R = glm::rotate(R, glm::radians(yaw), glm::vec3(glm::row(R, 1)));
-        R = glm::rotate(R, glm::radians(roll), {0, 0, -1});
+        R = glm::rotate(R, glm::radians(roll), {0, 0, 1});
         s.M_rot = R;
     }
 }
